1d0ce02012351130e85e27e52891e0a3ac149ae7
mohamedsalah2099 <mohamedsalah2099@gmail.com>
2021-01-25 20:11:14 +0200

    Implemented Connect and Drop Functions

---
+++ dbms.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Connecting to an Existing Database Function
+function connectDatabase 
+{
+
+  echo -e "Enter Database Name: \c"
+  read name
+
+  cd ./Database/$name 2> /dev/null
+
+  if [[ $? == 0 ]]
+	then
+    echo "Connected to $name Successfully"
+  else
+    echo "Database $name wasn't found"
+
+  fi
+}
+
+
+#Dropping an existing Database Function
+function dropDatabase {
+  echo -e "Enter Database Name: \c"
+  read name
+
+  rm -r ./Database/$name 2> /dev/null
+
+  if [[ $? == 0 ]]
+	then
+    echo "Successful Drop for $name Database"
+  else
+    echo "Database Not found"
+  
+ fi
+}
+
