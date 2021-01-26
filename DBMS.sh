127eaeea0b537c04860c52e056f032b2c5647090
MohamedBassiouny26 <mohamedsharf26797@gmail.com>
2021-01-26 18:34:56 +0200

    create table function

--- DBMS.sh
+++ DBMS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-arr=(Create List)
+arr=(Create List table)
 #main function
 function mainMenu {
 PS3="Enter a Choice : "
@@ -9,10 +9,11 @@ do
     case $REPLY in 
         1)  #create new database
             createDB
-            mainMenu
-            ;;  
+            mainMenu;;  
         2)  #List databases
-            listDBs;;  
+            listDBs;;
+        3)  #Create table
+            createTable;;    
         *) echo " Please Select from menu " ; mainMenu;
     esac
 done
@@ -30,6 +31,31 @@ function createDB {
 }
 function listDBs {
     ls
+}
+function createTable {
+    echo Please,Enter Table Name
+    read tableName
+    if [ -f $tableName ]
+    then
+        echo Table already exist
+        createTable
+    else
+        touch $tableName
+    fi
+    echo Enter number of columns
+    read colNum
+    typeset -i i=0
+    columns="name:type;"
+    while [ $i -lt $colNum ] 
+    do
+        echo Enter column Name
+        read colName
+        echo Enter coumn type
+        read colType
+        columns+=$colName:$colType;
+        i=$i+1
+    done
+    echo $columns>>$tableName;
 }
 mainMenu
 
